bcec3fe19325d5bef5b023a8caab08f7562ae374
skolson <skolson5903@gmail.com>
2026-03-29 16:17:23 -0700

    1.1.0 tests

--- kmp-sqlencrypt/src/androidDeviceTest/kotlin/com/oldguy/kiscmp/android/SqlCipherTests.kt
+++ kmp-sqlencrypt/src/androidDeviceTest/kotlin/com/oldguy/kiscmp/android/SqlCipherTests.kt
@@ -41,8 +41,8 @@ fun LocalDateTime.truncateToMillisecond(): LocalDateTime {
 
 @OptIn(ExperimentalTime::class)
 open class SqlCipherTests {
-    val sqlCipherVersion = "4.12.0 community"
-    val sqlite3Version = "3.51.1"
+    val sqlCipherVersion = "4.14.0 community"
+    val sqlite3Version = "3.51.3"
     val testDate = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).truncateToMillisecond()
     val testString = "Any text1 4"
     val testString2 = "Other text"
